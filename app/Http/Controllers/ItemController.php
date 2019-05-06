7c256ce21056ffaa5972553aa7062ea89cfacd3b
loveagri <984070691@qq.com>
2019-05-06 14:16:26 +0800

    link

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -46,7 +46,6 @@ class ItemController extends Controller
             'app_id' => 'required',
             'os' => 'required',
             'status' => 'required',
-            'link' => 'required',
         ]);
 
         $params = array_merge(request(['app_id', 'os', 'status', 'link']),['channel_id'=>request('channel_id') ?: '']);
