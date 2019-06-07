725f119ed7d1196c69249ec50507e6c2981cf16e
loveagri <984070691@qq.com>
2019-06-07 13:42:02 +0800

    os

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
         $os = $request->input('os', '');
         if ($os) {
--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -17,7 +17,7 @@ class ItemController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $os = $request->input('os', '');
         if ($os) {
