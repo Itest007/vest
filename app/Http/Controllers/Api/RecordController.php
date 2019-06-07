dcd1effa3e49ac4fa754f280e82acf5ec3ed1081
loveagri <984070691@qq.com>
2019-06-07 10:33:12 +0800

    record

--- app/Http/Controllers/Api/RecordController.php
+++ app/Http/Controllers/Api/RecordController.php
@@ -8,11 +8,19 @@ use App\Http\Controllers\Controller;
 
 class RecordController extends Controller
 {
+
+    public function index()
+    {
+        $items = Record::orderBy('created_at', 'desc')->paginate(10);
+         return response()->json( ['code' => 1, 'message' => 'Success','data'=>$items] );;
+    }
+
     public function record(Request $request)
     {
         $ip = $request->getClientIp();
         $rt = $request->input('rt', '') ?: '';
         $rj = $request->input('rj', json_encode([])) ?: json_encode([]);
+        $rj = json_encode($request->input('rj',[]) ?: []);
         $res = Record::create(compact('ip', 'rt', 'rj'));
 
         if ($res) {
@@ -20,6 +28,5 @@ class RecordController extends Controller
         }else{
              return response()->json( ['code' => 1, 'message' => 'faild'] );
         }
-
     }
 }
